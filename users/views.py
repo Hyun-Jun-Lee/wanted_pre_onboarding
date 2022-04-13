a2822d5e043f7522ab42f982673468392600110d
Lee Hyun Jun <mikey1000@naver.com>
2022-04-14 01:35:34 +0900

    <fix> : Migrations dependency issue
    Error code : "Migration admin.0001_initial is applied before its dependency users.0001_initial on database 'default'."
    
    db지우고 migration 파일 지운 후 다시 makemigrations
    앞으로 migrations 할 때 app 명시해주기

--- users/views.py
+++ users/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework import permissions
+from .models import User
+from .serializers import UserSerializer
+from .permissions import IsOwner
 
 # Create your views here.
+
+class UserViewSet(ModelViewSet):
+    
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    
+    # auth 권한
+    def get_permissions(self):
+        if self.action == "list" or self.action == "retrieve":
+            permission_classes = [permissions.AllowAny]
+        elif self.action == "create":
+            permission_classes = [permissions.IsAuthenticated]
+        else:
+            permission_classes = [IsOwner]
+        return [permission() for permission in permission_classes]
