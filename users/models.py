e5f303687323de8dbf98b7fb5a48bf9478cf0029
Lee Hyun Jun <mikey1000@naver.com>
2022-04-13 18:37:26 +0900

    <feature> : Create users app & models
    products.models를 개발하던 중 publisher 필드는 ForeginKey로 지정해야 함을
    깨닫고 users app & models 생성

---
+++ users/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+class User(AbstractUser):
+    
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+
+    GENDER_CHOICES = ((GENDER_MALE, "Male"), (GENDER_FEMALE, "Female"))
+    
+    name = models.CharField(max_length=30)
+    gender = models.CharField(
+        choices=GENDER_CHOICES, max_length=10, blank=True
+    )
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
