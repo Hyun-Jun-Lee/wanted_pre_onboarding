bafc9577c69c76fea0ddf07cdcce585e0ebfb929
Lee Hyun Jun <mikey1000@naver.com>
2022-04-13 18:59:56 +0900

    <fix> : ForeginKey 지정
    Product 모델에서 publisher 필드 foreginkey 지정

--- products/models.py
+++ products/models.py
@@ -1,11 +1,15 @@
 from django.db import models
+from users.models import User
 
 # Create your models here.
 
 class Product(models.Model):
+    
     title = models.CharField(max_length=30)
-    author = models.CharField(max_length=20)
     description = models.TextField()
     goal_amount = models.IntegerField()
     closing_date = models.DateTimeField()
     funding_amount = models.IntegerField()
+    
+    publisher = models.ForeignKey(User, related_name="products", on_delete=models.CASCADE)
+    
