6ef20cdfcb6e49ec8ea505ba0c5031eb145719d6
Lee Hyun Jun <mikey1000@naver.com>
2022-04-15 01:57:13 +0900

    <feature> : Add 'supporter' fields
    후원자 수 체크를 위해 supporter fields 생성 및 supporter_count 함수 작성

--- products/models.py
+++ products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from users.models import User
+import datetime
 
 # Create your models here.
 
@@ -8,8 +9,21 @@ class Product(models.Model):
     title = models.CharField(max_length=30)
     description = models.TextField()
     goal_amount = models.IntegerField()
-    closing_date = models.DateTimeField()
+    closing_date = models.DateField()
     funding_amount = models.IntegerField()
     
     publisher = models.ForeignKey(User, related_name="products", on_delete=models.CASCADE)
+    supporter = models.ManyToManyField("users.User")
+    
+    def supporter_count(self):
+        return self.supporter.count()
+    
+    def d_day(self):
+        now = datetime.date.today()
+        spplit = list(map(int,str(self.closing_date).split("-")))
+        target_day = datetime.date(spplit[0],spplit[1],spplit[2])
+        values = target_day - now
+        return values.days
+        
+        
     
