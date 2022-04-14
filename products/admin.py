3d53c559ce13babc4e881b8d4ccaf6f95f603feb
Lee Hyun Jun <mikey1000@naver.com>
2022-04-15 01:48:39 +0900

    <fix> : Customize Admin
    ModelAdmin 상속 받아서 display_list를 customize

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import Product
 # Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [
+            'title',
+            'description',
+            'goal_amount',
+            'closing_date',
+            'funding_amount',
+            'publisher',
+            'supporter_count',
+            'd_day'
+            ]
--- users/admin.py
+++ users/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 from .models import User
 # Register your models here.
 
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['pk','username','gender','is_staff']
