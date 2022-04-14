32a4c9c6c7649fb67e883adc52bb34f3ab53b667
Lee Hyun Jun <mikey1000@naver.com>
2022-04-15 00:03:19 +0900

    <fix> : Change Permission for UserViewset
    UserViewset에서 '삭제'와 '부분수정'은 인증된 유저만 가능하도록
    나머지는 모든 사용자가 가능하도록 permissions 수정

--- users/views.py
+++ users/views.py
@@ -11,13 +11,12 @@ class UserViewSet(ModelViewSet):
     
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    # permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     
-    # auth 권한
+    # # auth 권한
     def get_permissions(self):
-        if self.action == "list" or self.action == "retrieve":
-            permission_classes = [permissions.AllowAny]
-        elif self.action == "create":
-            permission_classes = [permissions.IsAuthenticated]
+        if self.action == "destroy" or self.action == "partial_update":
+            permission_classes = [permissions.IsAuthenticatedOrReadOnly]
         else:
-            permission_classes = [IsOwner]
+            permission_classes = [permissions.AllowAny]
         return [permission() for permission in permission_classes]
